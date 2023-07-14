9a534bc7471291a961cc9f26e08fc0ee65d41301
Vikram Navik <vikramnavik98@gmail.com>
2023-07-14 14:16:00 +0530

    change headers

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ require('dotenv').config();
 
 const app = express();
 
+// Middleware to enable CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+
 app.use(express.json());
 app.use(cors());
 
