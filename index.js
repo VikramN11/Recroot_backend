4168b1ea68bbd0573d238745ec5a6cb325f17982
Vikram Navik <vikramnavik98@gmail.com>
2023-07-14 14:22:11 +0530

    cors origin

--- index.js
+++ index.js
@@ -8,17 +8,13 @@ require('dotenv').config();
 
 const app = express();
 
-// Middleware to enable CORS
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
-
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  }));
 
 app.get("/", (req,res)=>{
     res.send("Home Page");
