1fe30a4b67fed5d4e99f233a2e98e4864b93e97f
Vikram Navik <vikramnavik98@gmail.com>
2023-07-14 14:06:58 +0530

    cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin:"*"}));
+app.use(cors());
 
 app.get("/", (req,res)=>{
     res.send("Home Page");
