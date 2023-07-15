170da289a0d51cab8514162ee9dafca73fdeaa3c
Vikram Navik <vikramnavik98@gmail.com>
2023-07-15 11:59:57 +0530

    *

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin: "https://64b10fe935ac18275ab23f1d--subtle-kangaroo-08802a.netlify.app"}));
+app.use(cors({origin: "*"}));
 
 app.get("/", (req,res)=>{
     res.send("Home Page");
