362e6d57f500dd86c330c3063cb1ec36b180feec
Vikram Navik <vikramnavik98@gmail.com>
2023-07-15 11:33:49 +0530

    required

--- model/User.model.js
+++ model/User.model.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
     name : {type:String, required:true},
     email : {type:String, required:true},
     password : {type:String, required:true},
-    profile : {type:String, required:false}
+    profile : {type:String, required:true}
 })
 
 const UserModel = mongoose.model("user", userSchema);
